85ec16e964342e94eb4f0194b21b0ab681090587
Stephen Mann <stman@microsoft.com>
2021-05-17 14:10:36 -0400

    Added console and web api demos.

---
+++ Console Demo/AppInsightsConsoleApp/AppInsightsConsoleApp/Logger.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+using Microsoft.ApplicationInsights;
+
+namespace AppInsightsConsoleApp
+{
+    public class Logger
+    {
+        private readonly TelemetryClient tc;
+        public Logger()
+        {
+            //Create a new telementry client with the app insights key you wish to target.
+            tc = new TelemetryClient();
+            tc.InstrumentationKey = "c04b54fd-429a-4359-8cfe-77c53c90175e";
+            tc.Context.Session.Id = Guid.NewGuid().ToString();
+            tc.Context.Device.OperatingSystem = Environment.OSVersion.ToString();
+        }
+
+        internal void Flush()
+        {
+            tc.Flush();
+            Thread.Sleep(1000);
+        }
+
+        public void LogInfo(string message)
+        {
+            tc.TrackTrace(message);
+        }
+
+        public void LogInfo(string message, IDictionary<string, string> properties)
+        {
+            tc.TrackTrace(message, properties);
+        }
+
+        public void LogError(Exception ex)
+        {
+            tc.TrackException(ex);
+        }
+
+        public void LogError(Exception ex, IDictionary<string, string> properties)
+        {
+            tc.TrackException(ex, properties);
+        }
+    }
+}
---
+++ Console Demo/AppInsightsConsoleApp/AppInsightsConsoleApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+
+namespace AppInsightsConsoleApp
+{
+    class Program
+    {
+        private static readonly Logger logger = new Logger();
+
+        static void Main(string[] args)
+        {
+            logger.LogInfo("Hello World!");
+
+            StartLongProcess();
+
+            FailingProcess();
+
+            logger.Flush();
+        }
+
+        private static void FailingProcess()
+        {
+            try
+            {
+                throw new Exception("Intentional exception");
+            }
+            catch(Exception ex)
+            {
+                logger.LogError(ex);
+            }
+        }
+
+        private static void StartLongProcess()
+        {
+            var sw = new Stopwatch(); 
+            sw.Start();
+            logger.LogInfo("Starting my long running task.");
+            Thread.Sleep(new Random().Next(1000, 6000));
+            sw.Stop();
+            var results = new Dictionary<string, string>();
+            results.Add("TaskTime", sw.ElapsedMilliseconds.ToString());
+            logger.LogInfo("Finished with long running task.", results);
+        }
+    }
+}
